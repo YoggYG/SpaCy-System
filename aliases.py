73d1231e8dfc235a4c8ac32c93323fe51561a808
Job <jobtalle@hotmail.com>
2018-06-04 11:52:29 +0200

    Aliases are properly stored

--- aliases.py
+++ aliases.py
@@ -1,11 +1,27 @@
+import json
+
 from os import listdir
 from os.path import isfile, join
 
 
 class Aliases:
+    class Set:
+        def __init__(self, property, aliases):
+            self.property = property
+            self.aliases = aliases
+
     DIRECTORY = "aliases/"
 
     def __init__(self):
-        files = [file for file in listdir(Aliases.DIRECTORY) if isfile(join(Aliases.DIRECTORY, file))]
+        self.sets = []
 
-        print(files)
+        for file in listdir(Aliases.DIRECTORY):
+            if isfile(join(Aliases.DIRECTORY, file)):
+                with open(join(Aliases.DIRECTORY, file)) as file:
+                    self.addFile(file)
+
+    def addFile(self, file):
+        data = json.load(file)
+        set = Aliases.Set(data["property"], data["aliases"])
+
+        self.sets.append(set)
